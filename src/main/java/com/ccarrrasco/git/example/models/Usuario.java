bfa4a0101a12fd652804dc04b797e71b37444e93
Cesar <ccarrascocarre@gmail.com>
2025-04-21 09:10:10 -0400

    Reapply "Primera Modificacion test"
    This reverts commit 37b9f20823cb246c614a990e83aaed2f966c9fc5.

--- src/main/java/com/ccarrrasco/git/example/models/Usuario.java
+++ src/main/java/com/ccarrrasco/git/example/models/Usuario.java
@@ -9,4 +9,12 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="user_id")
     private Long userId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
