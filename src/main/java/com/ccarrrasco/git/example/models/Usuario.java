f1e641550a0e0eec3cdb238efd493acbc8b9ba7e
Cesar <ccarrascocarre@gmail.com>
2025-04-21 08:48:13 -0400

    Genere la entidad Usuario

---
+++ src/main/java/com/ccarrrasco/git/example/models/Usuario.java
@@ -0,0 +1,12 @@
+package com.ccarrrasco.git.example.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long userId;
+}
